c5427a7a466f58ec39bf774c0c1db3249b3e3f7d
prabodh44 <prabodhtuladhar44@gmail.com>
2017-08-09 17:39:47 +0545

    Added button in index.html file

--- www/js/index.js
+++ www/js/index.js
@@ -44,3 +44,31 @@ var app = {
 };
 
 app.initialize();
+
+document.getElementById("setLocalStorage").addEventListener('click',setLocalStorage);
+document.getElementById("showLocalStorage").addEventListener('click',showLocalStorage);
+document.getElementById("removeProjectFromLocalStorage").addEventListener 
+   ('click', removeProjectFromLocalStorage); 
+document.getElementById("getLocalStorageByKey").addEventListener 
+   ('click', getLocalStorageByKey);  
+var localStorage = window.localStorage; 
+
+function setLocalStorage() {
+    localStorage.setItem("Name", "John");
+    localStorage.setItem("Job", "Developer");
+    localStorage.setItem("Project", "Cordova Project");
+}    
+
+function showLocalStorage() {
+    console.log(localStorage.getItem("Name"));
+    console.log(localStorage.getItem("Job"));
+    console.log(localStorage.getItem("Project"));
+}
+
+function removeProjectFromLocalStorage() {
+    localStorage.removeItem("Project");
+}
+
+function getLocalStorageByKey() {
+    alert("Inside the getLocalStorageByKey function");
+}
